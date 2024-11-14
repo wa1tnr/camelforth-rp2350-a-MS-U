5996d765010939cac245e01733ce645d24c94a8e
wa1tnr <wa1tnr@users.noreply.github.com>
2024-11-14 22:50:48 +0000

    recent updated tags for defines
      cosmetic only is the likely scope
    
    	modified:   forth_defines.h
    
    On branch dvlp-aa

--- 17-camelforth-b.d/pico-examples/camelforth-b/include/forth_defines.h
+++ 17-camelforth-b.d/pico-examples/camelforth-b/include/forth_defines.h
@@ -1,10 +1,10 @@
 // #warning seen 04 Apr 2023 forth_defines.h
 
 // RECENT_STAMP is for after a commit when there is a change
-#define RECENT_STAMP      "Sun Nov 10 19:37:53 UTC 2024"
-#define COMMIT_TIME_STAMP "Sun Nov 10 19:13:35 UTC 2024"
-#define BRANCH_STAMP      "dvlp-aa          r.00a- -Wall   0.2.0-pre-alpha"
-#define COMMIT_STAMP      "1a06375" // seven characters
+#define RECENT_STAMP      "Thu 14 Nov 20:48:39 UTC 2024"
+#define COMMIT_TIME_STAMP "Thu 14 Nov 20:43:52 UTc 2024"
+#define BRANCH_STAMP      "dvlp-aa  conic   r.00a- -Wall   0.2.0-pre-alpha"
+#define COMMIT_STAMP      "c6058d0" // seven characters
 #define FEATURE_STAMP     "+bugFixes +validInput +rp2350        "
 #define WOKWI_PRESENT -1
 
