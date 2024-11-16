3d77d90fb4fa2ba9ae40291fe1603d7a6539e792
wa1tnr <wa1tnr@users.noreply.github.com>
2024-11-16 20:24:59 +0000

    unfinished test to avoid include second time

--- 17-camelforth-b.d/pico-examples/camelforth-b/include/pico-hardware-camelforth.h
+++ 17-camelforth-b.d/pico-examples/camelforth-b/include/pico-hardware-camelforth.h
@@ -1,5 +1,5 @@
-#ifndef _PICO_HARDWARE_INCLUDES_LOCAL_CF
-#define _PICO_HARDWARE_INCLUDES_LOCAL_CF
+#ifndef _PICO_HARDWARE_INCLUDES_LOCAL_CF_
+#define _PICO_HARDWARE_INCLUDES_LOCAL_CF_
 #warning grafted back: pico-hardware-camelforth.h  Sat  9 Nov 19:27:45 UTC 2024
 // establish an ident string to print to console
 // establish flash limits
