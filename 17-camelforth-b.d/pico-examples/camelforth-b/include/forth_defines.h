8888012ecc277c28f124d0eb1d109c3bef99d617
wa1tnr <wa1tnr@users.noreply.github.com>
2024-11-16 20:24:24 +0000

    usual maintenance this file

--- 17-camelforth-b.d/pico-examples/camelforth-b/include/forth_defines.h
+++ 17-camelforth-b.d/pico-examples/camelforth-b/include/forth_defines.h
@@ -1,10 +1,15 @@
 // #warning seen 04 Apr 2023 forth_defines.h
 
 // RECENT_STAMP is for after a commit when there is a change
-#define RECENT_STAMP      "Thu 14 Nov 20:48:39 UTC 2024"
-#define COMMIT_TIME_STAMP "Thu 14 Nov 20:43:52 UTc 2024"
-#define BRANCH_STAMP      "dvlp-aa  conic   r.00a- -Wall   0.2.0-pre-alpha"
-#define COMMIT_STAMP      "c6058d0" // seven characters
-#define FEATURE_STAMP     "+bugFixes +validInput +rp2350        "
+#define RECENT_STAMP      "Sat 16 Nov 17:53:07 UTC 2024"
+#define COMMIT_TIME_STAMP "Sat 16 Nov 17:46:08 UTC 2024"
+#define BRANCH_STAMP      "dvlp-aa  MIR     r.00a- -Wall   0.2.0-pre-alpha"
+#define COMMIT_STAMP      "8bed31d" // seven characters
+// restore // #define FEATURE_STAMP     "+blipLED +nodots +verbose            "
+#define FEATURE_STAMP     "+crustacean +lockRobster +blipLED    "
 #define WOKWI_PRESENT -1
+
+// #define FEATURE_STAMP     "+bugFixes +validInput +rp2350        "
+// #define FEATURE_STAMP     "+flipperWIPaa +bugFixes +validInput  "
+// #define FEATURE_STAMP     "+verbose +flipperWIPaa +bugFixes     "
 
