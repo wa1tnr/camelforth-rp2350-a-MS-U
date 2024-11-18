65f2e8b23cae2567e773d17b42deebcd86b60668
wa1tnr <wa1tnr@users.noreply.github.com>
2024-11-18 01:55:05 +0000

    keeping up with id tags

--- 17-camelforth-b.d/pico-examples/camelforth-b/include/forth_defines.h
+++ 17-camelforth-b.d/pico-examples/camelforth-b/include/forth_defines.h
@@ -1,12 +1,13 @@
 // #warning seen 04 Apr 2023 forth_defines.h
 
 // RECENT_STAMP is for after a commit when there is a change
-#define RECENT_STAMP      "Sat 16 Nov 17:53:07 UTC 2024"
-#define COMMIT_TIME_STAMP "Sat 16 Nov 17:46:08 UTC 2024"
-#define BRANCH_STAMP      "dvlp-aa  MIR     r.00a- -Wall   0.2.0-pre-alpha"
-#define COMMIT_STAMP      "8bed31d" // seven characters
+
+#define RECENT_STAMP      "Mon 18 Nov 01:32:14 UTC 2024"
+#define COMMIT_TIME_STAMP "Sun 17 Nov 15:44:00 UTC 2024"
+#define BRANCH_STAMP      "dvlp-aa  table mesa  r.00a- -Wall   0.2.0-pre-alpha"
+#define COMMIT_STAMP      "80a4cd8" // seven characters
 // restore // #define FEATURE_STAMP     "+blipLED +nodots +verbose            "
-#define FEATURE_STAMP     "+crustacean +lockRobster +blipLED    "
+#define FEATURE_STAMP     "+basalt +garnet tourmaline +schist   "
 #define WOKWI_PRESENT -1
 
 // #define FEATURE_STAMP     "+bugFixes +validInput +rp2350        "
